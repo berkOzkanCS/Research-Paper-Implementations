7adac1f8687df5ec04df3e1ff7282bdb4973daf7
Berk Özkan <berkozkanjp@gmail.com>
2025-10-11 14:41:07 +0100

    astar

---
+++ A-star/UAV_Path_Planning_Based_on_Bidirectional_Sparse_A_Search_Algorithm/algo.cpp
@@ -0,0 +1,14 @@
+#include "sim/sim.hpp"
+#include "algo.hpp"
+
+// fills the waypoints/paths in map
+void Map::pathfind() {
+    std::cout << "\ncalling pathfind\n";
+
+    // pass acosrar bounds
+    
+
+    // path = std::move(algo.archive->solutions[0].waypoints);
+
+    std::cout << "\ncalled pathfind\n";
+}
